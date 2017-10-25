c866b7cadc08cfb4293de30f417f352258326ee3
unknown <mikeam42@gmail.com>
2017-10-25 13:32:37 -0500

    models

---
+++ ExploreMidwest/ExploreMidwest.Model/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreMidwest.Model
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public Category Category { get; set; }
+        public List<Tags> Tags { get; set; }
+        public string Body { get; set; }
+        public bool IsFinished { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

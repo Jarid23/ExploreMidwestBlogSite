af466aa08e1df79dd3026c49841ee3a93ce70a58
Ali Hirsi <ccfhaaji@yahoo.com>
2017-10-26 14:05:34 -0500

    Merge branch 'master' of https://bitbucket.org/exploremidwest/exploremidwest

--- ExploreMidwest/ExploreMidwest.Data/BlogRepositories/MockBlogRepo.cs
+++ ExploreMidwest/ExploreMidwest.Data/BlogRepositories/MockBlogRepo.cs
@@ -9,6 +9,115 @@ namespace ExploreMidwest.Data.BlogRepositories
 {
     public class MockBlogRepo : IBlogRepo
     {
+        private static List<Blog> _blogs = new List<Blog>
+        {
+            new Blog
+            {
+                BlogId = 1,
+                Title = "River Rafting",
+                Body = "What a great time it is rafting down the rapids in Minnesota. Going so fast makes it hard to keep a smile off my face",
+                Category = new Category
+                {
+                    CategoryId = 1,
+                    CategoryType = "Minnesota"
+                },
+                Tags = new List<Tags>
+                {
+                    new Tags
+                    {
+                        TagId = 1,
+                        TagName = "#Funtime"
+                    },
+                    new Tags
+                    {
+                        TagId = 2,
+                        TagName = "#Rivers"
+                    },
+                    new Tags
+                    {
+                        TagId = 3,
+                        TagName = "#Rafting"
+                    }
+                },
+                IsFinished = true,
+                IsDeleted = false,
+                Date = DateTime.Parse("10/21/2017"),
+            },
+            new Blog
+            {
+                BlogId = 2,
+                Title = "Corn Maze",
+                Body = "What a wonderful maze I found ",
+                Category = new Category
+                {
+                    CategoryId = 2,
+                    CategoryType = "Iowa"
+                },
+                Tags = new List<Tags>
+                {
+                    new Tags
+                    {
+                        TagId = 4,
+                        TagName = "#Coolplaces"
+                    },
+                    new Tags
+                    {
+                        TagId = 5,
+                        TagName = "#Outdoors"
+                    },
+                    new Tags
+                    {
+                        TagId = 1,
+                        TagName = "#Funtime"
+                    }
+                },
+                IsFinished = true,
+                IsDeleted = false,
+                Date = DateTime.Parse("8/19/2017"),
+            },
+            new Blog
+            {
+                BlogId = 3,
+                Title = "Football Game",
+                Body = "I went out to a cornhuskers game last night and it was a great time. The place was so packed and so loud everytime they scored thinking about it makes we want to go back right now!",
+                Category = new Category
+                {
+                    CategoryId = 3,
+                    CategoryType = "Nebraska"
+                },
+                Tags = new List<Tags>
+                {
+                    new Tags
+                    {
+                        TagId = 1,
+                        TagName = "#Goteam"
+                    },
+                    new Tags
+                    {
+                        TagId = 2,
+                        TagName = "#Bigcity"
+                    }
+                },
+                IsFinished = true,
+                IsDeleted = false,
+                Date = DateTime.Parse("10/15/2017"),
+            },
+            new Blog
+            {
+                BlogId = 4,
+                Title = "City Life",
+                Body = "Living in Minneapolis always has its challenges. It can be fun to ",
+                Category = new Category
+                {
+                    CategoryId = 1,
+                    CategoryType = "Minnesota"
+                },
+                Tags = new List<Tags>(),
+                IsFinished = false,
+                IsDeleted = false,
+                Date = DateTime.Parse("10/26/2017"),
+            }
+        };
         public List<Blog> GetNumberOfBlogs(int number, int set)
         {
             throw new NotImplementedException();
---
+++ ExploreMidwest/ExploreMidwest.Web/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using ExploreMidwest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExploreMidwest.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Blogs()
+        {
+            var model = BlogRepository.GetAll();
+            return View(model.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult AddBlog(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                BlogRepository.Add(blog.BlogId);
+                return RedirectToAction("Blog");
+            }
+            else
+            {
+                return View(new Blog());
+            }
+        }
+    }
+}

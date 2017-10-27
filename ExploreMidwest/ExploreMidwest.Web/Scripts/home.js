8a9bb3fff615496a4bfa4a8873126795fdd465e2
unknown <mikeam42@gmail.com>
2017-10-27 15:34:59 -0500

    linking

--- ExploreMidwest/ExploreMidwest.Web/Scripts/home.js
+++ ExploreMidwest/ExploreMidwest.Web/Scripts/home.js
@@ -38,3 +38,33 @@ function getNumber(number, sets){
         }
     })
 }
+
+function search() {
+    var para = $('#searchTerm').val();
+    var type = $('#searchCategory').val();
+    $.ajax({
+        url: 'http://localhost:8080/blog/' + type + '/' + para,
+        type: 'GET',
+        success: function (blogs) {
+            var output = "";
+            var i = 0;
+
+            for (i; i < blogs.length; i++) {
+                if (blogs[i].IsFinished) {
+                    output += '<div class="col-xs-3 blogDiv">Image</div>'
+                    output += '<div class="col-xs-9 blogDiv"><div class="col-xs-3 innerDiv"><h3>'
+                    output += blogs[i].Title + '</h3><br /><h4>'
+                    output += blogs[i].Category.CategoryType + '</h4><br />'
+                    output += blogs[i].Date + '</div>'
+                    output += '<div class="col-xs-9 innerDiv">'
+                    output += blogs[i].Body
+                    output += '<div style="text-align:end"><a href="#" id="det1">Read full article</a></div></div></div>'
+                }
+            }
+            $('#BlogsArea').html(output);
+        },
+        error: function (jqxhr, techstatus, errorthrow) {
+            $('#outputMessage').val("Error no products found!");
+        }
+    })
+}
